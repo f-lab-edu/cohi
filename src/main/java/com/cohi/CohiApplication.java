ab99cd3e8462783e8e449ace5cb745f8a4e07c98
“hingco” <hingco@kakao.com>
2024-05-14 00:12:53 +0900

    init commit

---
+++ src/main/java/com/cohi/CohiApplication.java
@@ -0,0 +1,13 @@
+package com.cohi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CohiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cohi/CohiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
